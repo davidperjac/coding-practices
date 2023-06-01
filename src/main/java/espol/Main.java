828f57912dcec94f7883dab47bbcc62ba9fd000f
wgcotera <wgcotera@espol.edu.ec>
2023-05-31 20:08:14 -0500

    chore: validate uder input

--- src/main/java/espol/Main.java
+++ src/main/java/espol/Main.java
@@ -8,19 +8,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Introduce el destino de la vacación:");
+        System.out.println("Introduce el destino de la vacación: ");
         String destination = scanner.nextLine();
 
-        System.out.println("Introduce el número de viajeros:");
-        int numTravelers = scanner.nextInt();
+        System.out.println("Introduce el número de viajeros: ");
+        int numberOfTravelers = scanner.nextInt();
 
-        System.out.println("Introduce la duración de la vacación en días:");
+        System.out.println("Introduce la duración de la vacación en días: ");
         int durationInDays = scanner.nextInt();
 
         try {
-            Destination destinationObj = new Destination(destination, 0, 0);
+            Destination destinationObj = new Destination(destination);
 
-            VacationPackage vacationPackage = new VacationPackage(numTravelers, destinationObj, durationInDays);
+            VacationPackage vacationPackage = new VacationPackage(numberOfTravelers, destinationObj, durationInDays);
 
             double cost = vacationPackage.calculateCost();
             System.out.println("El costo del paquete de vacaciones es: $" + cost);
@@ -29,5 +29,17 @@ public class Main {
         } catch (Exception e) {
             System.out.println("Error al calcular el costo del paquete de vacaciones. Puede ser un error de entrada.");
         }
+    }
+
+    private static boolean isValidInput(int numberOfTravelers, int durationInDays,  String destinationObj) {
+        if (numberOfTravelers <= 0 || durationInDays <= 0) {
+            return false;
+        }
+
+        if (destinationObj == null) {
+            return false;
+        }
+
+        return true;
     }
 }
